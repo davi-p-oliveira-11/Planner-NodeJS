71aa2c4d99d3c7f286a150c0091d70fed110d4eb
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-08-21 22:03:27 -0300

    added schema and sql table

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,30 @@
-console.log("Hello World !");
+import fastify from "fastify";
+import { prisma } from "./lib/prisma";
+
+const app = fastify();
+
+{
+  /* retake at 31:00 */
+}
+
+app.get("/cadastrar", async () => {
+  await prisma.trip.create({
+    data: {
+      destination: "Florianóplis",
+      starts_at: new Date(),
+      ends_at: new Date(),
+    },
+  });
+
+  return "Registro cadastado com sucesso !";
+});
+
+app.get("/listar", async () => {
+  const trips = await prisma.trip.findMany();
+
+  return trips;
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running");
+});
